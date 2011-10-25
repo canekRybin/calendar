75402b22da9d2e90da32b5da75e0146b181abb25
rybin alex <saneka.136@mail.ru>
2011-10-25 23:43:38 +0400

    ne udaetcja provesti vyborku za trkushij mesjac

--- app/controllers/qwerty_controller.rb
+++ app/controllers/qwerty_controller.rb
@@ -3,14 +3,15 @@ class QwertyController < ApplicationController
     
   end
   def my_events
-    @user = current_user#User.find(params[:id])
-    @events2 = current_user.events
+    @user = current_user
     months = {1=>"January",2 => "February", 3 => "March",4 => "April",
       5 => "May",6 => "June", 7 => "July",8 => "August",
       9 => "September",10 => "October", 11 => "November",12 => "December"}
     month_now = Time.now.month
     @month = months[month_now]
-    
+    first = Date.today.beginning_of_month
+    last = Date.today.end_of_month
+    @events = current_user.events.where("date >= #{first} and date <=#{last}")
     
    
   end
